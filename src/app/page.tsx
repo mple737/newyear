569831fa4189cc39ffc16eddede37f9b4d65108e
ArsenalBAck2004 <utphuongmap@gmail.com>
2024-12-31 23:14:57 -0500

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -98,9 +98,9 @@ export default function Home() {
             </h2>
             <p className="mt-4 text-xl text-gray-800">
               Wishing you all a year filled with love, laughter, and all
-              wonderful things. I’m proud of every one and
-              everything you’ve accomplished. Here’s to more amazing moments
-              together this year, and thank you for being an amazing
+              wonderful things. I’m proud of every single one of you and
+              everything yall have accomplished. Here's to more amazing moments
+              together this year, and thank you for being amazing
               friends. <br />
               <br />- Minh
             </p>
